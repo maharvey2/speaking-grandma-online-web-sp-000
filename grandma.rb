41743d14bb0b4ce8269b81c8485321691b3a5b1f
Mark Harvey <maharvey2@crimson.ua.edu>
2019-04-25 16:12:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,4 +15,10 @@ def speak_to_grandma(input)
   if input == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
   end
+elsif input != 'I LOVE YOU GRANDMA!'
+  return "HUH! SPEAK UP, SONNY!"
+else
+  return "NO, NOT SINCE 1938!"
+  end
 end
+  
